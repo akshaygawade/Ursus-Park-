423ffc1113dadbea50b87362559704297ffa0780
Akshay Gawade <akshaygawade73@gmail.com>
2019-10-24 15:15:02 +0530

    Data Binding commit

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,4 +1,16 @@
-import { LightningElement, track } from 'lwc';
-export default class HelloWebComponent extends LightningElement {
-	@track greeting = 'Trailblazer';
-}
+        import { LightningElement, track } from 'lwc';
+        export default class HelloWebComponent extends LightningElement {
+        @track greeting = 'Trailblazer';
+
+        handleGreetingChange(event) {
+            this.greeting = event.target.value;
+            
+
+        }
+        currentDate = new Date().toDateString();
+
+        get capitalizedGreeting() {
+        return `Hello ${this.greeting.toUpperCase()}!`;
+        }
+
+        }
